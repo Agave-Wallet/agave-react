d0924677fbf09ba38d2fa2044aaf1685e13719eb
saeveritt <saeveritt@gmail.com>
2020-03-29 07:59:42 -0500

    update sidebar

--- src/js/SideBarItem.js
+++ src/js/SideBarItem.js
@@ -1,19 +1,21 @@
 import React from "react";
-import "../css/SideBarItem.css";
+import "../css/SideBar.css";
 import Icons from '../img/sprite.svg';
 
-function SideBarItem(props) {
-    return (
-        <a href={"#"+props.name} className = "SideBarItem">
-            {/* Icon */}
-            <svg className="SideBarIcon">
-                <use href={`${Icons}#icon-${props.icon}`}></use>
-            </svg>
+class SideBarItem extends React.Component{
+    // constructor(props){
+    //     super(props)
+    // }
 
-            {/* Page Name */}
-            <span>{props.name}</span>
-        </a>
-    )
+    render(){
+        let a = <a href={"#"+this.props.name} className = "SideBarItem"><svg className="SideBarIcon">
+                <use href={`${Icons}#icon-${this.props.icon}`}></use></svg>
+                <span>{this.props.name}</span></a>
+        
+    return (   
+            a
+        )
+    };
 }
 
 export default SideBarItem;
