025412e37e7f55ddf7c359a0447b71cf2ff975a7
saeveritt <saeveritt@gmail.com>
2020-03-30 08:30:52 -0500

    update app container size

--- src/js/SideBarItem.js
+++ src/js/SideBarItem.js
@@ -8,10 +8,7 @@ class SideBarItem extends React.Component{
     //     super(props)
     // }s
 
-    render(){
-  
-        {/* let a = <a href={"#"+this.props.href} title={this.props.title} id={this.props.id} className="SideBarItem" onClick={this.props.onclick}><svg className="SideBarIcon"><use href={`${Icons}#icon-${this.props.icon}`} title={this.props.title}></use></svg></a> */}
-        
+    render(){   
     return (   
         <HashRouter>
         <NavLink exact to={this.props.href} activeClassName="SideBarItem active" onClick={this.props.onclick} className="SideBarItem"> <svg className="SideBarIcon"><use href={`${Icons}#icon-${this.props.icon}`} title={this.props.title}></use></svg>  </NavLink>
